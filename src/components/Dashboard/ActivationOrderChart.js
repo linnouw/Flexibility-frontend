de6512375c8259687a2ad592eba40149d58af508
linna98 <56513889+linna98@users.noreply.github.com>
2022-07-19 19:45:08 +0200

    apply linter changes

--- src/components/Dashboard/ActivationOrderChart.js
+++ src/components/Dashboard/ActivationOrderChart.js
@@ -1,60 +1,64 @@
-import React from 'react';
+import React from "react";
 // @Chartjs
-import Chart from 'chart.js/auto';
-import { Bar } from 'react-chartjs-2';
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 // @MUI
-import {Grid, Paper, Typography, Pagination} from '@mui/material';
+import { Grid, Paper, Typography, Pagination } from "@mui/material";
 // style
-import '../../App.css';
+import "../../App.css";
 import PropTypes from "prop-types";
 
 ActivationOrderChart.propTypes = {
-    labels: PropTypes.array.isRequired,
-    datas: PropTypes.array.isRequired
-}
+  labels: PropTypes.array.isRequired,
+  datas: PropTypes.array.isRequired,
+};
 
-export default function ActivationOrderChart({labels, datas}){
-  
-    const data={ 
-        labels,
-        datasets: [
-          {
-            label: 'Quantité',
-            data: datas,
-            borderColor: 'rgba(46, 125, 50)',
-            backgroundColor: 'rgba(46, 125, 50,0.5)',
-          },
-        ],
-    };
-    const options={
-        indexAxis: 'y',
-        elements: {
-        bar: {
-            borderWidth: 2,
-        },
-        },
-        responsive: true,
-        plugins: {
-        legend: {
-            position: 'bottom',
-            },
-        title: {
-            display: false,
-            text: 'Horizontal bar chart',
-            },
-        },
-    }
-    
-    return(
-        <Paper elevation={0} style={{borderRadius: 20, padding: 10}}>
-            <Grid container direction="column" justifyContent="center" alignItems="center">
-                <Grid item xs={2} m={1}>
-                    <Typography className="title">Activation order</Typography>
-                </Grid>
-                <Grid item xs={8} m={1}>
-                    <Bar data={data} options={options} width={400} height={170}/>
-                </Grid>
-            </Grid>
-        </Paper>
-    );
+export default function ActivationOrderChart({ labels, datas }) {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Quantity",
+        data: datas,
+        borderColor: "rgba(46, 125, 50)",
+        backgroundColor: "rgba(46, 125, 50,0.5)",
+      },
+    ],
+  };
+  const options = {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: false,
+        text: "Horizontal bar chart",
+      },
+    },
+  };
+
+  return (
+    <Paper elevation={0} style={{ borderRadius: 20, padding: 10 }}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={2} m={1}>
+          <Typography className="title">Activation order</Typography>
+        </Grid>
+        <Grid item xs={8} m={1}>
+          <Bar data={data} options={options} width={400} height={170} />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
--- src/components/Dashboard/BarChart.js
+++ src/components/Dashboard/BarChart.js
@@ -1,63 +1,73 @@
-import React from 'react';
+import React from "react";
 // @Chartjs
-import Chart from 'chart.js/auto';
-import { Bar } from 'react-chartjs-2';
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 // @MUI
-import {Grid, Paper, Typography, Pagination} from '@mui/material';
+import { Grid, Paper, Typography, Pagination } from "@mui/material";
 // style
-import '../../App.css';
+import "../../App.css";
 import PropTypes from "prop-types";
 
 BarChart.propTypes = {
-    title: PropTypes.string.isRequired,
-    labels: PropTypes.array.isRequired,
-    datas: PropTypes.array.isRequired,
-    borderColor: PropTypes.array.isRequired,
-    backgroundColor: PropTypes.array.isRequired
-}
+  title: PropTypes.string.isRequired,
+  labels: PropTypes.array.isRequired,
+  datas: PropTypes.array.isRequired,
+  borderColor: PropTypes.array.isRequired,
+  backgroundColor: PropTypes.array.isRequired,
+};
 
-export default function BarChart({title, labels, datas, borderColor, backgroundColor}) {
+export default function BarChart({
+  title,
+  labels,
+  datas,
+  borderColor,
+  backgroundColor,
+}) {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Quantity",
+        data: datas,
+        borderColor: borderColor,
+        backgroundColor: backgroundColor,
+      },
+    ],
+  };
+  const options = {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: false,
+        text: "Horizontal bar chart",
+      },
+    },
+  };
 
-    const data={ 
-        labels,
-        datasets: [
-          {
-            label: 'Quantité',
-            data: datas,
-            borderColor: borderColor,
-            backgroundColor: backgroundColor,
-          },
-        ],
-    };
-    const options={
-        indexAxis: 'y',
-        elements: {
-        bar: {
-            borderWidth: 2,
-        },
-        },
-        responsive: true,
-        plugins: {
-        legend: {
-            position: 'bottom',
-            },
-        title: {
-            display: false,
-            text: 'Horizontal bar chart',
-            },
-        },
-    }
-
-    return (         
-        <Paper elevation={0} style={{borderRadius: 10, padding: 10}}>
-            <Grid container direction="column" justifyContent="center" alignItems="center">
-                <Grid item xs={2} m={1}>
-                    <Typography className="title">{title}</Typography>
-                </Grid>
-                <Grid item xs={8} m={1}>
-                    <Bar data={data} options={options} width={400} height={170}/>
-                </Grid>
-            </Grid>
-        </Paper>
-    );
+  return (
+    <Paper elevation={0} style={{ borderRadius: 10, padding: 10 }}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={2} m={1}>
+          <Typography className="title">{title}</Typography>
+        </Grid>
+        <Grid item xs={8} m={1}>
+          <Bar data={data} options={options} width={400} height={170} />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
